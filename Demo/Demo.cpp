35f0613ebdec23a629598a98c3ecfb7060375fec
Denis Hilliard <armchaircommando@gmail.com>
2014-03-16 13:41:31 +0800

    Updated Demo app

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -1,4 +1,8 @@
 #include "..\openddl\detail.h"
+#include "..\openddl\detail\Error.h"
+#include "..\openddl\detail\Token.h"
+#include "..\openddl\detail\Command.h"
+
 #include <iostream>
 int main(int argc, char * argv[])
 {
@@ -6,7 +10,65 @@ int main(int argc, char * argv[])
 	using namespace openddl::detail;
 	std::vector<Token> tokens;
 	std::vector<Error> errors;
-	lex("float {1.0, 2.0, 3.0}", tokens, errors);
-	if (parse(tokens, errors))
-		std::cout << "Success" << std::endl;
+	std::vector<Command> commands;
+	if (!lex("float {3.0} int8 {99,22} ref {null}", tokens, errors))
+	{
+		std::cout << "Error during lexing" << std::endl;	
+	}	
+	else
+	{
+		for (auto & e : errors)
+			std::cout << e.message << std::endl;
+		if (!parse(tokens, commands, errors))
+		{
+			std::cout << "Error during parsing" << std::endl;
+			for (auto & e : errors)
+				std::cout << e.message << std::endl;
+		}
+		else
+		{
+			
+			if (errors.size() == 0)
+			{
+				std::cout << "Success" << std::endl;
+				std::string format("-- ");
+				for (auto & c : commands)
+				{
+					switch (c.type)
+					{
+					case Command::kDataList:
+						std::cout << "Got data list with "<< c.payload.list_.length << " elements."<< std::endl;
+						break;
+					case Command::kLiteral:
+					{
+						std::cout << format << "Got ";
+						switch (c.payload.literal_.encoding)
+						{
+						case Command::LiteralPayload::kBoolean:
+							std::cout << "boolean"; break;
+						case Command::LiteralPayload::kFloat:
+							std::cout << "float"; break;
+						case Command::LiteralPayload::kInteger:
+							std::cout << "integer"; break;
+						case Command::LiteralPayload::kReference:
+							std::cout << "reference"; break;
+						case Command::LiteralPayload::kString:
+							std::cout << "string"; break;
+						case Command::LiteralPayload::kType:
+							std::cout << "type"; break;
+						}
+						std::cout <<" Literal" << std::endl;
+					}
+						
+						
+					}
+				}
+			}
+			else
+				for (auto & e : errors)
+					std::cout << e.message << std::endl;
+		}
+	
+	}
+	
 }
--- openddl/detail.h
+++ openddl/detail.h
@@ -6,103 +6,38 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <cstdint>
 namespace openddl
 {
+
+	enum Type {
+		kFloat,
+		kDouble,
+		kRef,
+		kUnsignedInt8,
+		kUnsignedInt16,
+		kUnsignedInt32,
+		kUnsignedInt64,
+		kInt8,
+		kInt16,
+		kInt32,
+		kInt64,
+		kBool,
+		kString,
+		kType
+	};
 	namespace detail
 	{
-		struct Token
-		{
-			enum token_t
-			{
-				kLeftSquareBracket = 1,
-				kRightSquareBracket,
-				kComma,
-				kLeftBrace,
-				kRightBrace,
-				kEquals,
-				kIdentifier,
-				kName,
-				kNull,
-				kBooleanLiteral,
-				kFloatLiteral,
-				kHexLiteral,
-				kDecimalLiteral,
-				kBinaryLiteral,
-				kStringLiteral,
-				kCharacterLiteral,
-				kFloat,
-				kDouble,
-				kUnsignedInt8,
-				kUnsignedInt16,
-				kUnsignedInt32,
-				kUnsignedInt64,
-				kInt8,
-				kInt16,
-				kInt32,
-				kInt64,
-				kRef,
-				kBool,
-				kString,
-				kType
-			};
 
-			bool is_float_encoded() const;
-			bool is_integer_encoded() const;
-			bool is_data_type() const;
-
-			token_t		token_type;
-
-			std::string payload;
-
-			unsigned int range_start;
-			unsigned int range_length;
-
-			unsigned int line;
-
-		};
-		inline bool has_intervening_whitespace(const Token & a, const Token & b) { return (a.range_start + a.range_length) != b.range_start; }
-		struct Error
-		{
-
-			std::string payload;				//The literal (as string) which caused the error
-			std::string message;				//Plain text key describing error		
-
-			unsigned int range_start;			//Position of offset of offending literal in string
-			unsigned int range_length;
-		};
+		struct Token;
+		struct Error;
+		struct Command;
 
 		//Will only return false if an internal error has occurred and not all input was consumed.
 		bool lex(const std::string & input, std::vector<Token> &tokens, std::vector<Error> & errors);
 		//Will only return false if an internal error has occurred and not all input was consumed.
-		bool parse(const std::vector<Token> & tokens, std::vector<Error> & errors);
+		bool parse(const std::vector<Token> & tokens, std::vector<Command> & commands, std::vector<Error> & errors);
 
-
-		struct LexerContext
-		{
-			std::vector<Error> & errors;
-			std::vector<Token> & tokens;
-			const std::string & input;
-		private:
-			int line;
-			char const * line_start;
-			int characters_consumed;
-
-		public:
-			LexerContext(const std::string & _i, std::vector<Token> & _t, std::vector<Error> & _e);
-
-			void reset_count() { characters_consumed = 0; }
-			void count_character() { characters_consumed++; }
-			int get_character_count() { return characters_consumed; }
-
-			void new_line(const char * p);
-
-			void lex_error(const std::string & name, unsigned int ts, unsigned int te);
-			void lex_error(const std::string & name, unsigned int ts, char const* te);
-			void lex_error(const std::string & name, char const* ts, unsigned int te);
-			void lex_error(const std::string & name, const char * ts, const char* te);
-
-			void lex_emit(Token::token_t t_type, const char* ts, const char* te);
-		};
 	}
 
 
